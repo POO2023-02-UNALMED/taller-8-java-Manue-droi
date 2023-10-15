29b316091e8ac01c1fe6dd8abff7017d3de239ad
Manue-droi <manusarf16@gmail.com>
2023-10-15 14:57:11 -0500

    m

--- src/main/java/futbol/Portero.java
+++ src/main/java/futbol/Portero.java
@@ -12,7 +12,7 @@ public class Portero extends Futbolista {
 	}
 	public String toString() {
 		return 
-			super.toString()+" con el dorsal "+this.dorsal+". Le han marcado "+this.golesRecibidos;
+			super.toString()+" con el dorsal "+this.getDorsal()+". Le han marcado "+this.getGolesRecibidos();
 	}
 	public int compareTo(Portero p){
 		 Portero otroPortero = (Portero) p;
