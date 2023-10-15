ab461def313f1f5fbeae66396c2289fc2c41579f
Manue-droi <manusarf16@gmail.com>
2023-10-15 13:01:59 -0500

    hkhk

--- src/main/java/Futbolista/Portero.java
+++ src/main/java/Futbolista/Portero.java
@@ -1,12 +1,12 @@
 package Futbolista;
 
-public class Portero extends Futbolista{
+public class Portero extends Futbolista {
 	public short golesRecibidos;
 	public byte dorsal;
 	public final String posicion = "portero";
 	
 	public Portero() {
-		this("Maradona", 30,"delantero", 289, 7);
+		this("Maradona", 30,"delantero", (short) 289, (byte)7);
 	}
 	public Portero(String nombre, int edad, String posicion, short golesRecibidos, byte dorsal) {
 		super(nombre, edad, posicion);
@@ -14,24 +14,12 @@ public class Portero extends Futbolista{
 		this.dorsal = dorsal;
 	}
 	public String toString() {
-		return "El futbolista" + this.getNombre() + " tiene "+ this.etEdad() + "juega de " + this.getPosicion() + "con el dorsal" + getDorsal() + ". Le han marcado" + getGolesRecibidos();
+		return "El futbolista" + this.getNombre() + " tiene "+ this.getEdad() + "juega de " + this.getPosicion() + "con el dorsal" + this.dorsal() + ". Le han marcado" + this.golesRecibidos();
 	}
 	public int compareTo(Portero p){
         return Math.abs(this.golesRecibidos-p.golesRecibidos);
 	}
 	boolean jugarConLasManos() {
 		return true;
-	}
-	public int getGolesRecibidos() {
-		return golesRecibidos;
-	}
-	public void setGolesRecibidos(short golesRecibidos) {
-		this.golesRecibidos = golesRecibidos;
-	}
-	public int getDorsal() {
-		return dorsal;
-	}
-	public void setDorsal(byte dorsal) {
-		this.dorsal = dorsal;
 	}
 }
--- src/main/java/futbol/Jugador.java
+++ src/main/java/futbol/Jugador.java
@@ -5,17 +5,18 @@ public class Jugador extends Futbolista {
 	public byte dorsal;
 	
 	public Jugador() {
-		this("Maradona", 30, "delantero", 289, 7);
+		this("Maradona", 30, "delantero", (short)289, (byte)7);
 	}
-	public boolean jugarConLasManos();
 	
 	public Jugador( String nombre,int edad, String posicion, short golesMarcados, byte dorsal) {
 		super( nombre,edad, posicion);
 		this.golesMarcados = golesMarcados;
 		this.dorsal = dorsal;
 	}
+	
+	
 	public String toString() {
-		return "El futbolista" + this.getNombre() + " tiene " + this.getEdad() + "juega de " + this.getPosicion() + "con el dorsal" + this.dorsal()+ ". Ha marcado" + this.dorsal();
+		return "El futbolista" + this.getNombre() + " tiene " + this.getEdad() + " juega de " + this.getPosicion() + " con el dorsal " + this.dorsal()+ ". Ha marcado " + this.dorsal();
 	}
     public int compareTo(Futbolista f){
         return Math.abs(this.getEdad()-f.getEdad());
