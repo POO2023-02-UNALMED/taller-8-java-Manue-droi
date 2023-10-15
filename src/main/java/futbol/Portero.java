6e98a9650f52c867c1a1c00d0766cfa6f1e513f1
Manue-droi <manusarf16@gmail.com>
2023-10-15 13:34:52 -0500

    YO DIGO Q AHORA SI

--- src/main/java/futbol/Portero.java
+++ src/main/java/futbol/Portero.java
@@ -4,7 +4,7 @@ public class Portero extends Futbolista {
 	public short golesRecibidos;
 	public byte dorsal;
 	
-	public Portero(String nombre, int edad, String posicion, short golesRecibidos, byte dorsal) {
+	public Portero(String nombre, int edad, short golesRecibidos, byte dorsal) {
 		super(nombre, edad, "portero");
 		this.golesRecibidos = golesRecibidos;
 		this.dorsal = dorsal;
