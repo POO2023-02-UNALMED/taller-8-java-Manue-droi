eddcf50ef03874460a35393c9c00780e4e3d8e7f
Manue-droi <manusarf16@gmail.com>
2023-10-15 14:54:21 -0500

    fhfh

--- src/main/java/futbol/Portero.java
+++ src/main/java/futbol/Portero.java
@@ -4,10 +4,7 @@ public class Portero extends Futbolista {
 	public short golesRecibidos;
 	public byte dorsal;
 	
-	public Portero() {
-		super("Maradona",30,"Portero");
-	}
-
+	
 	public Portero(String nombre, int edad, short golesRecibidos, byte dorsal) {
 		super(nombre, edad, "portero");
 		this.golesRecibidos = golesRecibidos;
@@ -15,7 +12,7 @@ public class Portero extends Futbolista {
 	}
 	public String toString() {
 		return 
-			super.toString()+"con el dorsal "+this.dorsal+". Le han marcado"+this.golesRecibidos;
+			super.toString()+" con el dorsal "+this.dorsal+". Le han marcado "+this.golesRecibidos;
 	}
 	public int compareTo(Portero p){
 		 Portero otroPortero = (Portero) p;
