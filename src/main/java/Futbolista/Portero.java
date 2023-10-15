7bfa690bb7ebba837fa454e8aa687ed14c59f2f3
Manue-droi <manusarf16@gmail.com>
2023-10-15 13:10:20 -0500

    ere

--- src/main/java/Futbolista/Portero.java
+++ src/main/java/Futbolista/Portero.java
@@ -3,18 +3,17 @@ package Futbolista;
 public class Portero extends Futbolista {
 	public short golesRecibidos;
 	public byte dorsal;
-	public final String posicion = "portero";
 	
 	public Portero() {
 		this("Maradona", 30,"delantero", (short) 289, (byte)7);
 	}
 	public Portero(String nombre, int edad, String posicion, short golesRecibidos, byte dorsal) {
-		super(nombre, edad, posicion);
+		super(nombre, edad, "portero");
 		this.golesRecibidos = golesRecibidos;
 		this.dorsal = dorsal;
 	}
 	public String toString() {
-		return "El futbolista" + this.getNombre() + " tiene "+ this.getEdad() + "juega de " + this.getPosicion() + "con el dorsal" + this.dorsal() + ". Le han marcado" + this.golesRecibidos();
+		return "El futbolista" + this.getNombre() + " tiene "+ this.getEdad() + "juega de " + this.getPosicion() + "con el dorsal" + this.dorsal + ". Le han marcado" + this.golesRecibidos;
 	}
 	public int compareTo(Portero p){
         return Math.abs(this.golesRecibidos-p.golesRecibidos);
